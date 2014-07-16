c8d07da4afd1a0fadad628eb655a647cf77ed1c1
yanpqi <yanpqi@gmail.com>
2014-07-16 23:28:05 +0800

    add full-path when gen lookupfile tag file ,sometimes relative path may cause vim can not use lookupfile to open file after the first open.

--- speeder/project.sh
+++ speeder/project.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # generate tag file for lookupfile plugin
 echo -e "!_TAG_FILE_SORTED\t2\t/2=foldcase/" > filenametags
-find . -not -regex '.*\.\(png\|gif\)' -type f -printf "%f\t%p\t1\n" | \
+find $PWD -not -regex '.*\.\(png\|gif\)' -type f -printf "%f\t%p\t1\n" | \
     sort -f >> filenametags 
 # generate tags file for ctags
 ctags -R --fields=+iaS --extra=+q --c++-kinds=+px *
